88fc9a52c3ef329b683d68c93817f5e134357f3c
yjiahao123 <yjiahao123@gmail.com>
2025-01-04 20:57:05 +0800

    update app after refactoring code

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import uvicorn
 
 import pandas as pd
 import numpy as np
@@ -8,9 +9,9 @@ from joblib import load
 
 from chronos import ChronosPipeline
 
-from utils.arimax import preprocess_arima_data, arimax_predict
-from utils.xgb import preprocess_xgb_data, xgb_predict
-from utils.chronos import chronos_predict
+from utils.arimax import Arimax
+from utils.xgb import XGB
+from utils.chronos import Chronos
 
 # instantiate an instance of the app
 app = FastAPI()
@@ -23,32 +24,35 @@ class PredData(BaseModel):
     days: int
 
 # load the models
-ARIMAX = load("../models/arimax_model.pkl")
-XGB = load("../models/best_XGBRegressor_v1.pkl")
-CHRONOS_T5_SMALL = ChronosPipeline.from_pretrained("amazon/chronos-t5-small")
+arimax = Arimax("../models/arimax_model.pkl")
+xgb = XGB("../models/best_XGBRegressor_v1.pkl")
+chronos_t5_small = Chronos("amazon/chronos-t5-small")
 
 # post endpoints to get predictions from the models
 @app.post("/arimax")
 def arima_preds(data: PredData):
     # preprocess data into the form that the ARIMAX model accepts
-    df_arima = preprocess_arima_data(df)
+    df_arima = arimax.preprocess_data(df)
     # use arimax to predict the future prices and confidence intervals
-    predicted_prices, conf_int = arimax_predict(ARIMAX, df_arima, data.days)
+    predicted_prices, conf_int = arimax.predict(df_arima, data.days)
     return {"preds": predicted_prices, "conf_int": conf_int}
 
 # post endpoint for XGB
 @app.post("/XGB")
 def xgb_preds(data: PredData):
-    df_xgb = preprocess_xgb_data(df)
+    df_xgb = xgb.preprocess_data(df)
     # use XGB to predict the future prices
-    predicted_prices = xgb_predict(XGB, df_xgb, data.days).tolist()
+    predicted_prices = xgb.predict(df_xgb, data.days).tolist()
     return {"preds": predicted_prices}
 
 # post endpoint for the chronos model
 @app.post("/chronos")
 def chronos_preds(data: PredData):
-    low, median, high = chronos_predict(CHRONOS_T5_SMALL, df, data.days)
+    low, median, high = chronos_t5_small.predict(df, data.days)
     low = low.tolist()
     median = median.tolist()
     high = high.tolist()
     return {"preds": median, "low": low, "high": high}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
